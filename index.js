d8c65f03f85d943f2d06fd29e7a74bdba3af6e10
edwinacos55 <edwinacos55@gmail.com>
2022-12-14 16:03:34 -0500

    commit tree ae103fc76c94b2560dad955cf744238f16cb0349

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Code your solutions in this file
+// for (let age=30; age < 40; age++) {
+//     console.log(`I'm ${age} years old. Happy Birthday to me!`);
+//     debugger;
+// }
+
+// const gifts = ['teddy bear', 'drone', 'doll'];
+
+// function wrapGifts(gifts) {
+//     for ( let i=0; i < gifts.length; i++){
+        // console.log(`Wrapped ${gifts[i]} and added a bow!`);
+//         debugger;
+//     }
+//     return gifts;
+// }
+
+
+
+function writeCards(names, surprise) {
+    let message= [];
+    for (let i=0; i< names.length; i++ ) {
+    message.push(`Thank you, ${names[i]}, for the wonderful ${surprise} gift!`);
+    
+    }
+    return message;
+}
+
+function countDown(number) {
+    let i= number;
+    while (i >= 0 ) {
+    console.log(i);
+    i--;
+    }
+return number;
+}
+
